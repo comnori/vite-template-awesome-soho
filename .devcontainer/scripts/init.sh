6d81bb1add11c0443195faa5587051be00c89c90
윤용식 <comnori@gmail.com>
2023-12-24 23:06:55 +0900

    Update devcontainer.json, init.sh, initPackageJson.sh, and 1 more file

--- .devcontainer/scripts/init.sh
+++ .devcontainer/scripts/init.sh
@@ -1,1 +1,37 @@
-sudo chsh -s /bin/zsh $(whoami)
+#/bin/bash
+
+git config --global init.defaultBranch main
+
+sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)" -- \
+  -t jonathan \
+  -p git \
+  -p ssh-agent \
+  -p yarn \
+  -p gatsby \
+  -p https://github.com/zsh-users/zsh-autosuggestions \
+  -p https://github.com/zsh-users/zsh-completions
+
+corepack enable
+
+yarn config set --home enableTelemetry 0
+yarn install
+
+yarn global add gatsby-cli
+
+cat << EOF >> ~/.zshrc
+
+# set PATH so it includes user's yarn global bin if it exists
+if [ -d "\$HOME/.yarn/bin" ] ; then
+  PATH="\$(yarn global bin):\$PATH"
+fi
+EOF
+
+mkdir -p ~/.config/gatsby
+
+cat << EOF >> ~/.config/gatsby/config.json
+{
+  "cli": {
+    "packageManager": "yarn",
+  }
+}
+EOF
