25c046f837b3208fdfaffa6cf57e2f5c09d90ebd
윤용식 <comnori@gmail.com>
2023-12-28 16:23:21 +0900

    Update init.sh

--- .devcontainer/scripts/init.sh
+++ .devcontainer/scripts/init.sh
@@ -6,14 +6,13 @@ sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1
   -p git \
   -p ssh-agent \
   -p yarn \
-  -p gatsby \
   -p https://github.com/zsh-users/zsh-autosuggestions \
   -p https://github.com/zsh-users/zsh-completions \
   -p https://github.com/zsh-users/zsh-syntax-highlighting \
   -x
 
 sh -c "corepack enable"
-
 sh -c "yarn config set --home enableTelemetry 0"
+sh -c "yarn set version berry"
 sh -c "yarn install"
-
+sh -c "yarn dlx @yarnpkg/sdks vscode"
