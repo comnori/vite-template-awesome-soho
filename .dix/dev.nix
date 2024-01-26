d9ccc0388581d5493cbb6d800693aaff3b93109a
comnori <comnori@gmail.com>
2024-01-26 10:15:24 +0000

    Nix Configuration test

---
+++ .dix/dev.nix
@@ -0,0 +1,47 @@
+{ pkgs, ... }: {
+
+  channel = "stable-23.11";
+
+  packages = [
+    pkgs.nodejs_20
+  ];
+
+  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+  idx.extensions = [
+    "aaron-bond.better-comments",
+    "chrisbibby.hide-node-modules",
+    "christian-kohler.npm-intellisense",
+    "dbaeumer.vscode-eslint",
+    "mikestead.dotenv",
+    "eamodio.gitlens",
+    "EditorConfig.EditorConfig",
+    "esbenp.prettier-vscode",
+    "Gruntfuggly.todo-tree",
+    "formulahendry.auto-rename-tag",
+    "liamhammett.inline-parameters",
+    "lokalise.i18n-ally",
+    "meganrogge.template-string-converter",
+    "mhutchie.git-graph",
+    "donjayamanne.githistory",
+    "oderwat.indent-rainbow",
+    "PKief.material-icon-theme",
+    "SonarSource.sonarlint-vscode",
+    "usernamehw.errorlens",
+    "semanticdiff.semanticdiff"
+  ];
+
+  idx.previews = {
+    enable = true;
+    previews = [
+      {
+        command = [
+          "npm"
+          "run"
+          "start"
+        ];
+        manager = "web";
+        id = "web";
+      }
+    ];
+  };
+}
